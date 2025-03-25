ec5cc6bbc17edff891a907c725d8fbf18f867843
Artem Son <s0sis0n@yandex.ru>
2025-03-25 11:38:55 +0300

    updates

--- home/hyprland/suspend.sh
+++ home/hyprland/suspend.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 swayidle -w \
-timeout 120 ' swaylock ' \
+# timeout 120 ' swaylock ' \
 timeout 400 ' hyprctl dispatch dpms off' \
 timeout 12000 'systemctl suspend' \
 resume ' hyprctl dispatch dpms on' \
-before-sleep 'swaylock'
+# before-sleep 'swaylock'
